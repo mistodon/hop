838e4fa94e5c62472a30a23a5563f0a332934726
Vi <1149443+mistodon@users.noreply.github.com>
2016-09-16 17:33:32 +0100

    WIP raw bash implementation

--- hop.sh
+++ hop.sh
@@ -41,3 +41,44 @@ function _hop_complete() {
 }
 
 complete -F _hop_complete hop
+
+function hopp() {
+    local commandname="hop"
+    local subcommand=$1
+    shift
+
+    if [ "$subcommand" == "to" ]; then
+        local bookmark_name=$1
+        if [ -z "$bookmark_name" ]; then
+            echo "usage: $commandname $subcommand bookmark_name" 1>&2
+            return 1
+        fi
+
+        local bookmarks=$(grep "${bookmark_name}:" ~/.hop/hopp_bookmarks | cut -d ":" -f 2)
+        if [ -n "$bookmarks" ]; then
+            cd $bookmarks
+        else
+            echo "hop: no bookmark found with name $bookmark_name" 1>&2
+        fi
+        return 0
+
+    elif [ "$subcommand" == "add" ]; then
+        local bookmark_name=$1
+        if [ -z "$bookmark_name" ]; then
+            echo "usage: $commandname $subcommand bookmark_name" 1>&2
+            return 1
+        fi
+        echo "${bookmark_name}:`pwd`" >> ~/.hop/hopp_bookmarks
+        return 0
+        
+    else
+        echo "usage: $commandname <command>" 1>&2
+        echo "" 1>&2
+        echo "Commands:" 1>&2
+        echo "  add     Add a bookmark to the current directory" 1>&2
+        echo "  to      Change working directory to a bookmarked directory" 1>&2
+        return 1
+    fi
+
+    return 0
+}
